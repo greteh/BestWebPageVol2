14f56a19bcbb9aff73e27f428bc02791f26d09e8
greteh <Gretetiiger27>
2021-10-25 00:04:58 +0300

    more dropdown menu

--- res/js/jsIndex.js
+++ res/js/jsIndex.js
@@ -38,11 +38,11 @@ $(function dropdownMenu(){
 });
 
 window.onclick = function(event) {
-    if (!event.target.matches(".account")) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
+    if (!event.target.matches(".dropbtn")) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            let openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
